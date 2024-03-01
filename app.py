f9f904a49a0c731f62bf238029e7d805593dee84
Willy <willy@git.com>
2024-03-01 23:11:59 +0100

    update

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/bestaetigung')
+def bestaetigung():
+    return render_template('bestellung.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
